7ca2940ff9239c1d744ee86e4a32bd87a1e4e536
ShinMini <gusals121234@gmail.com>
2023-01-27 15:12:49 +0900

    [update] relate to tailwind css config

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import '@css'
 import ChartView from '@/screens/ChartView'
 
 function App() {
-
 	return (
 		<div className="App">
-			<ChartView />
+			<nav className="flex text-4xl bg-slate-600 p-5 m-auto">
+				<div className="mx-10 cursor-pointer hover: text-blue-300">sample 1</div>
+				<div className="  mx-10">sample 2</div>
+				<div className="  mx-10">sample 3</div>
+			</nav>
+			<h1 className="text-3xl font-bold underline"> Hello world! </h1>
+			{/* <ChdivrtView className='ml-5'/> */}
 		</div>
 	)
 }
