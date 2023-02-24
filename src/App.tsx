75e2e00c053fc4d49a3734d611d3ff83158e4cf5
ShinMini <gusals121234@gmail.com>
2023-02-24 15:42:41 +0900

    fix: update to release version

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import ChartView from '@/screens/ChartView'
 function App() {
 	return (
 		<div className="App">
-			<nav className="flex text-4xl bg-slate-600 p-5 m-auto">
-				<div className="mx-10 cursor-pointer hover: text-blue-300">sample 1</div>
-				<div className="  mx-10">sample 2</div>
-				<div className="  mx-10">sample 3</div>
-			</nav>
-			<h1 className="text-3xl font-bold underline"> Hello world! </h1>
-			{/* <ChdivrtView className='ml-5'/> */}
+			{/*<nav className="flex text-4xl bg-slate-600 p-5 m-auto">*/}
+			{/*	<div className="mx-10 cursor-pointer hover: text-blue-300">sample 1</div>*/}
+			{/*	<div className="  mx-10">sample 2</div>*/}
+			{/*	<div className="  mx-10">sample 3</div>*/}
+			{/*</nav>*/}
+			{/*<h1 className="text-3xl font-bold underline"> Hello world! </h1>*/}
+			<ChartView />
 		</div>
 	)
 }
